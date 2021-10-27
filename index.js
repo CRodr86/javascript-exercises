e525786bcc3c1f3e324c2da28aa5f8fa2a7ce861
CRodr86 <rodrigocfotografia@gmail.com>
2021-10-27 18:25:15 +0000

    First commit

---
+++ index.js
@@ -0,0 +1,3 @@
+let surnames = [“10”, “juan”, “@12”, “null”, “antonioPerezDelCarmen”, “abcdefghtioiasoisdjads”, “Manolo”, “Perez”, “Soledad”];
+let escuses = [“OMG?“, “What’s going on?“, “How much is it?“, “18”, null, ‘undefined’, function(){}];
+let = [“Jeferson”, “Matilda”, “R@fael”, “1van”, “Pep3", “Loquesea”, “Fel1berto”, “Pepit@“, “D@M”];
